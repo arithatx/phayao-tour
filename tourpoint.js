92dd827133fe5e2fe70b3ddb5c21e8eb894287e3
arithatx <124546062+arithatx@users.noreply.github.com>
2023-04-24 11:25:42 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,158 @@
+var tourpoint = 
+    
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "กว๊านพะเยา",
+          "type": "natural",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            99.87608857014055,
+            19.169281294838115
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกภูซาง",
+          "type": "natural",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.44490419210797,
+            19.586934999525894
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานเเห่งชาติเเม่ปืม",
+          "type": "natural",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            99.91257831839346,
+            19.430566048088224
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานเเห่งชาติดอยภูนาง",
+          "type": "natural",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.16867457349383,
+            19.00999202685027
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานเเห่งชาติภูลังกา",
+          "type": "natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.51053086483051,
+            19.357192690814585
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระนั่งดิน",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.32423860585129,
+            19.49859078069875
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บ้านสวนคอฟฟี่",
+          "type": "special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.32985230122438,
+            19.4837499319228
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อ่างเก็บน้ำเเม่ต๋ำ",
+          "type": "natural",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            99.94800202601579,
+            19.007344022556893
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดร่องคำหลวง",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            99.84893480053216,
+            19.04539004527696
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ดอยหนอก",
+          "type": "culture",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.74590144047727,
+            19.14125897839206
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
